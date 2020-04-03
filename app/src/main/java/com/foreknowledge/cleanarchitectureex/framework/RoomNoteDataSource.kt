d5ce02a26a6070a938fd093ddb42cfb972e8e643
s2choco <kyj_0129@naver.com>
2020-04-03 14:48:54 +0900

    add RoomNoteDataSource

---
+++ app/src/main/java/com/foreknowledge/cleanarchitectureex/framework/RoomNoteDataSource.kt
@@ -0,0 +1,19 @@
+package com.foreknowledge.cleanarchitectureex.framework
+
+import android.content.Context
+import com.foreknowledge.cleanarchitectureex.framework.db.DatabaseService
+import com.foreknowledge.cleanarchitectureex.framework.db.NoteEntity
+import com.foreknowledge.core.data.Note
+import com.foreknowledge.core.repository.NoteDataSource
+
+class RoomNoteDataSource(context: Context): NoteDataSource {
+    private val noteDao = DatabaseService.getInstance(context).noteDao()
+
+    override suspend fun add(note: Note) = noteDao.addNoteEntity(NoteEntity.fromNote(note))
+
+    override suspend fun get(id: Long): Note? = noteDao.getNoteEntity(id)?.toNote()
+
+    override suspend fun getAll(): List<Note> = noteDao.getAllNoteEntities().map { it.toNote() }
+
+    override suspend fun remove(note: Note) = noteDao.deleteNoteEntity(NoteEntity.fromNote(note))
+}
