41ddc407f67e6e6b445b79dd4579a3195e739682
s2choco <kyj_0129@naver.com>
2020-04-03 12:59:38 +0900

    make NoteFragment

---
+++ app/src/main/java/com/foreknowledge/cleanarchitectureex/presentation/NoteFragment.kt
@@ -0,0 +1,24 @@
+package com.foreknowledge.cleanarchitectureex.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.foreknowledge.cleanarchitectureex.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class NoteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note, container, false)
+    }
+
+}
