9b41b013bb183054c8b03dc9d2719d62e08b11ed
s2choco <kyj_0129@naver.com>
2020-04-03 16:15:53 +0900

    ListViewModel 추가

---
+++ app/src/main/java/com/foreknowledge/cleanarchitectureex/framework/ListViewModel.kt
@@ -0,0 +1,34 @@
+package com.foreknowledge.cleanarchitectureex.framework
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.foreknowledge.core.data.Note
+import com.foreknowledge.core.repository.NoteRepository
+import com.foreknowledge.core.usecase.AddNote
+import com.foreknowledge.core.usecase.GetAllNotes
+import com.foreknowledge.core.usecase.GetNote
+import com.foreknowledge.core.usecase.RemoveNote
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ListViewModel(application: Application): AndroidViewModel(application) {
+    private val coroutineScrope = CoroutineScope(Dispatchers.IO)
+
+    val repository = NoteRepository(RoomNoteDataSource(application))
+
+    val useCases = UseCases(
+        AddNote(repository),
+        GetNote(repository),
+        GetAllNotes(repository),
+        RemoveNote(repository)
+    )
+
+    val noteList = MutableLiveData<List<Note>>()
+
+    fun getAllNotes() =
+        coroutineScrope.launch {
+            noteList.postValue(useCases.getAllNotes())
+        }
+}
