a9d9d8010a26dd268dbbb25b3b7ba76bc3a27a9f
s2choco <kyj_0129@naver.com>
2020-04-03 16:09:53 +0900

    NoteListAdapter 생성

---
+++ app/src/main/java/com/foreknowledge/cleanarchitectureex/presentation/NoteListAdapter.kt
@@ -0,0 +1,44 @@
+package com.foreknowledge.cleanarchitectureex.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.foreknowledge.cleanarchitectureex.R
+import com.foreknowledge.core.data.Note
+import kotlinx.android.synthetic.main.item_note.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class NoteListAdapter(private val notes: MutableList<Note>): RecyclerView.Adapter<NoteListAdapter.NoteViewHolder>() {
+
+    fun updateNotes(newNotes: MutableList<Note>) {
+        notes.clear()
+        notes.addAll(newNotes)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder =
+        NoteViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, false))
+
+    override fun getItemCount(): Int = notes.size
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) =
+        holder.bind(notes[position])
+
+    inner class NoteViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        private val noteLayout = view.noteLayout
+        private val noteTitle = view.title
+        private val noteContent = view.content
+        private val noteDate = view.date
+
+        fun bind(note: Note) {
+            noteTitle.text = note.title
+            noteContent.text = note.content
+
+            val sdf = SimpleDateFormat("MMM dd, HH:mm:ss", Locale.getDefault())
+            val resultDate = "Last updated: ${sdf.format(Date(note.updateTime))}"
+            noteDate.text = resultDate
+        }
+    }
+}
